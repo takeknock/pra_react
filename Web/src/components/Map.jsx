d07ce475aa3bf2fd5f5f3cadb0952bd6bab9c485
mmatthew <takeknock@gmail.com>
2018-02-27 23:42:38 +0900

    add moving function

--- Web/src/components/Map.jsx
+++ Web/src/components/Map.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
+import propTypes from 'prop-types';
+
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
 const InnerMap = withGoogleMap(props => (
   <GoogleMap
     defaultZoom={15}
-    defaultCenter={{ lat: -8.7792696, lng: 115.1668263 }}
+    defaultCenter={props.position}
+    center={props.position}
   />
 ));
 
-const Map = props => (
+const Map = ({ lat, lng }) => (
   <InnerMap
     containerElement={<div />}
     mapElement={<div className="map" />}
+    position={{ lat, lng }}
   />
 );
+
+Map.propTypes = {
+  lat: propTypes.number,
+  lng: propTypes.number,
+};
+
+Map.defaultProps = {
+  lat: -8.7792696,
+  lng: 115.1668263,
+};
 
 export default Map;
