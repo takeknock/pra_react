d40dc9f5ba8dbada6799b35cc7177051528b4917
mmatthew <takeknock@gmail.com>
2018-02-27 23:50:39 +0900

    add marker

--- Web/src/components/Map.jsx
+++ Web/src/components/Map.jsx
@@ -8,16 +8,24 @@ const InnerMap = withGoogleMap(props => (
     defaultZoom={15}
     defaultCenter={props.position}
     center={props.position}
-  />
+  >
+    <Marker
+      {...props.marker}
+    />
+  </GoogleMap>
 ));
 
-const Map = ({ lat, lng }) => (
-  <InnerMap
-    containerElement={<div />}
-    mapElement={<div className="map" />}
-    position={{ lat, lng }}
-  />
-);
+const Map = ({ lat, lng }) => {
+  const position = { lat, lng };
+  return (
+    <InnerMap
+      containerElement={<div />}
+      mapElement={<div className="map" />}
+      position={position}
+      marker={{ position }}
+    />
+  );
+};
 
 Map.propTypes = {
   lat: propTypes.number,
